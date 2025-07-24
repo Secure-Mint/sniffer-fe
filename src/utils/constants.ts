03ae53d7a3c5179df33412def7d7540c11824035
Murad Malik <muradmalik7@gmail.com>
2025-07-24 22:34:01 +0700

    handled no user nfts design

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,6 +5,6 @@ export const ENTER = "Enter";
 export const ENTER_KEY_CODE = 13;
 export const SNIFFER_DISCLAIMER = `The audit score reflects how well a token aligns with common safety criteria, 
   based on automated analysis. 
-  However, automated scanners are not foolproof — a high score does not guarantee the absence of malicious behavior. 
+  However, automated scanners are not foolproof and high score does not guarantee the absence of malicious behavior. 
   This score is not financial advice and should be evaluated alongside other sources. 
   Always conduct your own research before making any decisions.`;
