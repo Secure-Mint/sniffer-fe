f92230cc22dda299fb8f2d7858840b714b41ef00
Murad Malik <muradmalik7@gmail.com>
2025-11-19 18:53:33 +0400

    updated loader design

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -4,7 +4,7 @@ const Loader = () => {
       {/* Loader Wrapper */}
       <div className='relative w-24 h-24 flex items-center justify-center'>
         {/* Outer Sweep Progress Ring */}
-        <div className='absolute w-full h-full rounded-full border-4 border-gray-600/20'>
+        <div className='absolute w-full h-full rounded-full border-4 border-gray-500/20'>
           <div className='absolute inset-0 rounded-full border-4 border-transparent border-t-gray-300 animate-sweep'></div>
         </div>
 
@@ -13,7 +13,7 @@ const Loader = () => {
 
         {/* Inner hologram core */}
         <div className='relative w-14 h-14 flex items-center justify-center'>
-          <div className='absolute inset-0 rounded-full bg-gray-600/20 blur-xl'></div>
+          <div className='absolute inset-0 rounded-full bg-gray-500/20 blur-xl'></div>
 
           <div className='w-10 h-10 rounded-full bg-gradient-to-br from-gray-200 to-gray-500 shadow-[0_0_25px_8px_rgba(160,160,160,0.5)] animate-pulse' />
         </div>
